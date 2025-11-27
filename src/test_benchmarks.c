195149264db3b0376690a125ea9d303440e0c2ec
skakked <skakked@gmail.com>
2025-11-26 20:50:28 -0500

    Add benchmark lookup performance

--- src/test_benchmarks.c
+++ src/test_benchmarks.c
@@ -52,3 +52,52 @@ BenchmarkResult benchmark_insertion(int *keys, int n, size_t capacity) {
     return result;
 }
 
+// Benchmark lookup performance
+BenchmarkResult benchmark_lookup(int *keys, int n, size_t capacity) {
+    BenchmarkResult result = {0, 0, 0, 0};
+    double start, end;
+    int val;
+    
+    // Build all tables with the same data first
+    ChainedHashMap *ch = chained_create(capacity);
+    LinearHashMap *lh = linear_create(capacity);
+    CuckooHashMap *cu = cuckoo_create(capacity);
+    
+    for (int i = 0; i < n; i++) {
+        chained_put(ch, keys[i], i);
+        linear_put(lh, keys[i], i);
+        cuckoo_put(cu, keys[i], i);
+    }
+    
+    // Benchmark Chained lookups
+    start = get_time_ms();
+    for (int i = 0; i < n; i++) {
+        chained_get(ch, keys[i], &val);
+    }
+    end = get_time_ms();
+    result.chained_ms = end - start;
+    
+    // Benchmark Linear Probing lookups
+    start = get_time_ms();
+    for (int i = 0; i < n; i++) {
+        linear_get(lh, keys[i], &val);
+    }
+    end = get_time_ms();
+    result.linear_ms = end - start;
+    
+    // Benchmark Cuckoo lookups
+    start = get_time_ms();
+    for (int i = 0; i < n; i++) {
+        cuckoo_get(cu, keys[i], &val);
+    }
+    end = get_time_ms();
+    result.cuckoo_ms = end - start;
+    result.cuckoo_rehashes = cuckoo_rehash_count(cu);
+    
+    chained_destroy(ch);
+    linear_destroy(lh);
+    cuckoo_destroy(cu);
+    
+    return result;
+}
+
