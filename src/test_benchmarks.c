96acda71b0070a33c19c41087ec93547e9b77b2c
skakked <skakked@gmail.com>
2025-11-26 20:48:30 -0500

    Add benchmark insertion performance

--- src/test_benchmarks.c
+++ src/test_benchmarks.c
@@ -13,3 +13,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Benchmark insertion performance
+BenchmarkResult benchmark_insertion(int *keys, int n, size_t capacity) {
+    BenchmarkResult result = {0, 0, 0, 0};
+    double start, end;
+    
+    // Benchmark Chained HashMap insertion
+    ChainedHashMap *ch = chained_create(capacity);
+    start = get_time_ms();
+    for (int i = 0; i < n; i++) {
+        chained_put(ch, keys[i], i);
+    }
+    end = get_time_ms();
+    result.chained_ms = end - start;
+    chained_destroy(ch);
+    
+    // Benchmark Linear Probing insertion
+    LinearHashMap *lh = linear_create(capacity);
+    start = get_time_ms();
+    for (int i = 0; i < n; i++) {
+        linear_put(lh, keys[i], i);
+    }
+    end = get_time_ms();
+    result.linear_ms = end - start;
+    linear_destroy(lh);
+    
+    // Benchmark Cuckoo insertion
+    CuckooHashMap *cu = cuckoo_create(capacity);
+    start = get_time_ms();
+    for (int i = 0; i < n; i++) {
+        cuckoo_put(cu, keys[i], i);
+    }
+    end = get_time_ms();
+    result.cuckoo_ms = end - start;
+    result.cuckoo_rehashes = cuckoo_rehash_count(cu);
+    cuckoo_destroy(cu);
+    
+    return result;
+}
+
